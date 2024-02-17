7487e0b79d0a832f407015070823c5773b1a68ec
Elissa-DI <elissadusabe@gmail.com>
2024-02-17 21:06:43 +0200

    started login page

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useWarmUpBrowser } from '@/hooks/useWarmupBrowser'
 
 const Page = () => {
+  useWarmUpBrowser();
+  
   return (
     <View>
       <Text>Login</Text>
